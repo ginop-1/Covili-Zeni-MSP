7d5f00e9af987d60258cdded5c7d2afa228acedc
lindazeni <linda.zeni@fermi.mo.it>
2022-05-08 17:42:30 +0200

    add crossplatform

--- msp_reader/runner.py
+++ msp_reader/runner.py
@@ -1,5 +1,7 @@
 from .MQTTListener import MQTTListener
 from .SerialReader import SerialReader
+
+import serial.tools.list_ports
 from threading import Thread
 import json
 
@@ -9,8 +11,16 @@ def run(config_file: str = "config.json"):
     with open(config_file, "r") as f:
         config = json.load(f)["MQTT"]
 
+    ports = serial.tools.list_ports.comports()
+
+    print("Available ports:")
+    [print(f"{ix}. {port.device} {port.description}") for ix, port in enumerate(ports)]
+    
+    port = ports[int(input("Choose port (use index): "))].device
+
+
     mqtt_listener = MQTTListener("mqtt", config["topic"], config["broker"])
-    serial_reader = SerialReader("serial", config["topic"], config["broker"])
+    serial_reader = SerialReader("serial", config["topic"], config["broker"], port)
     listener_thread = Thread(target=mqtt_listener.run)
     serial_thread = Thread(target=serial_reader.run)
     try:
